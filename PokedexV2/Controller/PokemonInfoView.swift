fc5c19a1b7a2c04aca476a1a6a0fed61e6192066
Diogo Filipe Abreu Rodrigues <rodrigues.diogofilipe@gmail.com>
2024-04-02 19:07:23 +0100

    layout to pokemonInfoView Set um

--- PokedexV2/Controller/PokemonInfoView.swift
+++ PokedexV2/Controller/PokemonInfoView.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class PokemonInfoView: UIViewController {
 
+    
+    //layout Constraits
+    
+    @IBOutlet weak var speciesInfoWidth: NSLayoutConstraint!
+    @IBOutlet weak var speciesInfoHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var abilitiesWidth: NSLayoutConstraint!
+    @IBOutlet weak var abilitesHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var baseInfoHeight: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+            super.willTransition(to: newCollection, with: coordinator)
+                if UIDevice.current.orientation.isLandscape {
+                    changeLayout(0.5, 0.6, 0.5, 0.6, 0.4)
+                } else {
+                    changeLayout(1, 0.4, 1, 0.4, 0.2)
+                }
     }
-    */
-
+    
+    func changeLayout(_ w1: CGFloat, _ h1: CGFloat, _ w2: CGFloat, _ h2: CGFloat, _ w3: CGFloat ) {
+        speciesInfoWidth = speciesInfoWidth.setMultiplier(multiplier: w1)
+        speciesInfoHeight = speciesInfoHeight.setMultiplier(multiplier: h1)
+        abilitiesWidth = abilitiesWidth.setMultiplier(multiplier: w2)
+        abilitesHeight = abilitesHeight.setMultiplier(multiplier: h2)
+        baseInfoHeight = baseInfoHeight.setMultiplier(multiplier: w3)
+        view.layoutIfNeeded()
+    }
 }
