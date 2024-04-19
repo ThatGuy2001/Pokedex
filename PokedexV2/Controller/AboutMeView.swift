3de10f26728d7bcc5f5a6fa4ec129151be2e12d5
Diogo Filipe Abreu Rodrigues <rodrigues.diogofilipe@gmail.com>
2024-04-19 10:20:51 +0100

    about me responds to layout orientation

--- PokedexV2/Controller/AboutMeView.swift
+++ PokedexV2/Controller/AboutMeView.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class AboutMeView: UIViewController {
+    
+    var textLabel : UILabel?
+    var textBox : UIVisualEffectView?
+    var image : UIImageView?
+    
+    var imageCenterConstrait : NSLayoutConstraint?
+    var imageMarginConstrait : NSLayoutConstraint?
+    var labelLeadingConstrait : NSLayoutConstraint?
+    var labelTopConstrait : NSLayoutConstraint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +39,8 @@ class AboutMeView: UIViewController {
     
     func addTextBox(){
         let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
-        let textBox = UIVisualEffectView(effect: blurEffect)
+        textBox = UIVisualEffectView(effect: blurEffect)
+        guard let textBox else {return}
         textBox.layer.cornerRadius = 50
         textBox.clipsToBounds = true
         view.addSubview(textBox)
@@ -42,7 +52,8 @@ class AboutMeView: UIViewController {
         
      // addTextLabel
         
-        let textLabel = UILabel()
+        textLabel = UILabel()
+        guard let textLabel else { return  }
         textLabel.text = """
 This app was devoped by Diogo Rodrigues as a part of a internship in DigitalWorks.
 
@@ -55,25 +66,85 @@ All the app was designed using a storyboard or xib files, with the exeption of t
         
         textBox.contentView.addSubview(textLabel)
         textLabel.translatesAutoresizingMaskIntoConstraints = false
-        textLabel.bottomAnchor.constraint(equalTo: textBox.contentView.bottomAnchor, constant: -50).isActive = true
-        textLabel.trailingAnchor.constraint(equalTo: textBox.contentView.trailingAnchor,constant: -50).isActive = true
-        textLabel.topAnchor.constraint(equalTo: textBox.contentView.topAnchor, constant: 210).isActive = true
-        textLabel.leadingAnchor.constraint(equalTo: textBox.contentView.leadingAnchor, constant: 50).isActive = true
+        textLabel.bottomAnchor.constraint(equalTo: textBox.contentView.bottomAnchor, constant: -30).isActive = true
+        textLabel.trailingAnchor.constraint(equalTo: textBox.contentView.trailingAnchor,constant: -30).isActive = true
+        
+        labelTopConstrait = textLabel.topAnchor.constraint(equalTo: textBox.contentView.topAnchor, constant: 210)
+        labelTopConstrait?.isActive = true
+        
+        labelLeadingConstrait = textLabel.leadingAnchor.constraint(equalTo: textBox.contentView.leadingAnchor, constant: 30)
+        labelLeadingConstrait?.isActive = true
         
     // add profileImage
         
         let contentview = textBox.contentView
         
-        let image = UIImageView(image: UIImage(named: "AboutMe"))
+        image = UIImageView(image: UIImage(named: "AboutMe"))
+        guard let image else { return }
         contentview.addSubview(image)
         image.layer.cornerRadius = 20
         image.clipsToBounds = true
         image.translatesAutoresizingMaskIntoConstraints = false
         
-        image.topAnchor.constraint(equalTo: contentview.topAnchor, constant: 10 ).isActive = true
-        image.centerXAnchor.constraint(equalTo: contentview.centerXAnchor , constant: 0).isActive = true
+        imageCenterConstrait = image.centerXAnchor.constraint(equalTo: contentview.centerXAnchor , constant: 0)
+        imageCenterConstrait?.isActive = true
+        
+        imageMarginConstrait = image.topAnchor.constraint(equalTo: contentview.topAnchor, constant: 10 )
+        imageMarginConstrait?.isActive = true
         
         image.widthAnchor.constraint(equalToConstant: 200).isActive = true
         image.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        changeLayouyt()
+    }
+    
+    
+    
+    func changeLayouyt() {
+        guard var imageCenterConstrait else { return  }
+        guard var imageMarginConstrait  else { return  }
+        guard var labelTopConstrait else { return  }
+        guard var labelLeadingConstrait else { return }
+        guard let contentview = textBox?.contentView  else { return }
+        guard let image else { return }
+        guard let textLabel else { return }
+        
+        imageCenterConstrait.isActive = false
+        imageMarginConstrait.isActive = false
+        labelTopConstrait.isActive = false
+        labelLeadingConstrait.isActive = false
+        
+            if UIDevice.current.orientation.isLandscape {
+                self.imageCenterConstrait = image.centerYAnchor.constraint(equalTo: contentview.centerYAnchor , constant: 0)
+                self.imageCenterConstrait?.isActive = true
+                
+                self.imageMarginConstrait = image.leadingAnchor.constraint(equalTo: contentview.leadingAnchor, constant: 10 )
+                self.imageMarginConstrait?.isActive = true
+                
+                self.labelTopConstrait = textLabel.topAnchor.constraint(equalTo: contentview.topAnchor, constant: 30)
+                self.labelTopConstrait?.isActive = true
+                
+                self.labelLeadingConstrait = textLabel.leadingAnchor.constraint(equalTo: contentview.leadingAnchor, constant: 220)
+                self.labelLeadingConstrait?.isActive = true
+            } else {
+                self.imageCenterConstrait = image.centerXAnchor.constraint(equalTo: contentview.centerXAnchor , constant: 0)
+                self.imageCenterConstrait?.isActive = true
+                
+                self.imageMarginConstrait = image.topAnchor.constraint(equalTo: contentview.topAnchor, constant: 10 )
+                self.imageMarginConstrait?.isActive = true
+                
+                self.labelTopConstrait = textLabel.topAnchor.constraint(equalTo: contentview.topAnchor, constant: 210)
+                self.labelTopConstrait?.isActive = true
+                
+                self.labelLeadingConstrait = textLabel.leadingAnchor.constraint(equalTo: contentview.leadingAnchor, constant: 50)
+                self.labelLeadingConstrait?.isActive = true
+                
+            }
+        
+    }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+            super.willTransition(to: newCollection, with: coordinator)
+            changeLayouyt()
     }
 }
